5b75de41728cc6640bccc286101d6d4b1f59522e
SanTa45zgz <756308@unizar.es>
2020-10-10 11:58:17 +0200

    Migration from Java to Kotlin - TravisCI conf added

--- src/main/kotlin/es/unizar/webeng/hello/Application.kt
+++ src/main/kotlin/es/unizar/webeng/hello/Application.kt
@@ -10,11 +10,7 @@ open class Application : SpringBootServletInitializer() {
     override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
         return application.sources(Application::class.java)
     }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
 }
+    fun main(args: Array<String>) {
+        SpringApplication.run(Application::class.java, *args)
+    }
